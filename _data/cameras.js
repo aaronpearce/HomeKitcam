586e134c413e83cb5a002c8482efd5c4a031750e
aaronpearce <aaronpearce@users.noreply.github.com>
2024-03-02 22:11:24 +0000

    deploy: 0a698294f8d4875219def68fb4f008c508ae37fa

--- _data/cameras.js
+++
@@ -1,9 +0,0 @@
-const client = require('../utils').contentfulClient;
-
-module.exports = async () => {
-  const posts = await client.getEntries({
-	content_type: 'camera',
-	order: '-sys.createdAt',
-  });
-  return posts.items;
-};
--- utils.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const contentful = require('contentful');
-
-const contentfulClient = contentful.createClient({
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
-  space: process.env.CTF_SPACE_ID,
-});
-
-exports.contentfulClient = contentfulClient;
