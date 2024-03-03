f31233ea411dd9a03cb7e6a29e773abc5e0bdf45
Aaron Pearce <me@aaronpearce.com>
2024-03-03 15:44:40 +1300

    Fix images

--- _data/cameras.js
+++ _data/cameras.js
@@ -3,7 +3,7 @@ const client = require('../utils').contentfulClient;
 module.exports = async () => {
   const cameras = await client.getEntries({
 	content_type: 'camera',
-	order: '-sys.publishedAt',
+	order: '-sys.createdAt',
   });
   return cameras.items;
 };
