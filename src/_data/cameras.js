74666a7b6bf828c5cfe221e72d40e17855850b43
Aaron Pearce <me@aaronpearce.com>
2024-04-05 15:35:11 +1300

    Fix

--- src/_data/cameras.js
+++ src/_data/cameras.js
@@ -5,5 +5,9 @@ module.exports = async () => {
   const cameras = await client.getEntries({
 	content_type: 'camera'
   });
-  return arrayShuffle(cameras.items);
+
+  return camera.items
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
 };
